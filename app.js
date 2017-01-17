a8489d7c2c9464ff9a32987e2208158057b51ded
tnsherpa1 <tnsherpa1@gmail.com>
2017-01-17 12:11:32 -0800

    seves favicon locally

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.engine('hbs', expHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '/', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
